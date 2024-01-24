af96796608bc40748334e8dbae02a4832fc0abe9
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-01-24 12:35:24 +0000

    Create SpecifiedExpression.java

---
+++ SpecifiedExpression.java
@@ -0,0 +1,17 @@
+// Day 1 
+
+/*
+Write a Java program to find the value of a specified expression.
+a) 101 + 0) / 3
+b) 3.0e-6 * 10000000.1
+c) true && true
+d) false && true
+e) (false && false) || (true && true)
+f) (false || false) && (true && true)
+*/
+
+public class SpecifiedExpression {
+    public static void main(String[] args) {
+        
+    }
+}
